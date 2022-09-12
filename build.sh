5b15fab0f58356d282051a3eb958cade5ffb15e4
kangwoosukeq <kangwoosuk1@gmail.com>
2022-09-13 08:21:21 +0900

    [build] update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# install dependencies
+sudo apt-get update
+sudo apt-get install -y g++ cmake autoconf libgmp-dev libmpfr-dev libsqlite3-dev m4 pkg-config zlib1g-dev gcc make software-properties-common
+
+sudo add-apt-repository ppa:avsm/ppa
+sudo apt-get update
+sudo apt-get install -y opam
+
+# init submodule
+git submodule init
+git submodule update
+
 # build infer
 pushd tracer-infer
 NINJA=no ./build-infer.sh clang -y
 popd
 
 # build ranking system
+eval $(opam env)
 pushd rank
 make
 popd
